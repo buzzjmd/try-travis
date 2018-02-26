6ec48be8850c7ab98cfd77bbcc60b056a8afcfc1
buzzjmd <buzzjmd@gmail.com>
2018-02-26 13:14:24 +0000

    Add Darwin to platform test

--- tests/test_pythons.py
+++ tests/test_pythons.py
@@ -3,7 +3,7 @@
 """Python specific tests."""
 
 
-def test_pythons():
+def test_python_versions():
     """Test Pythons.
 
     Use this test to check coverage,
@@ -16,9 +16,19 @@ def test_pythons():
     else:
         # Python 3 or later
         assert sys.version_info.major >= 3
+
+
+def test_platforms():
+    """Test Platforms.
+
+    Use this test to check coverage,
+    i.e. make sure both windows and linux have been tested.
+    """
     import platform
-    _platform = platform.system() 
-    if _platform == "Linux":
-        assert _platform == "Linux"
-    elif _platform == "Windows":
+    _platform = platform.system()
+    if _platform == "Windows":
         assert _platform == "Windows"
+    elif _platform == "Darwin":
+        assert _platform == "Darwin"
+    else:
+        assert _platform == "Linux"
